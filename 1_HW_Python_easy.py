bb6fa18185c4cce864303bceac8bccd880d39aec
maksyutovrv <maksyutovrv@ya.ru>
2019-01-25 00:27:44 +0300

    Отформатировано

--- 1_HW_Python_easy.py
+++ 1_HW_Python_easy.py
@@ -1,21 +1,21 @@
-#Easy level
-#Task 1
+# Easy level -
+# Task 1
 a = 10
 b = '19'
 c = 3.7
-print(a,b,c)
+print(a, b, c)
 
-name = input ('Enter any value: ')
+name = input('Enter any value: ')
 print(name)
 
-#Task 2
+# Task 2
 value = int(input('Enter any value: '))
 value = value + 2
 print(value)
 
-#Task 3
-age = input ('Please enter your age: ')
+# Task 3
+age = input('Please enter your age: ')
 if age >= '18':
     print('Access granted')
 else:
-    print ('Sorry, access allowed from the age 18')
+    print('Sorry, access allowed from the age 18')
